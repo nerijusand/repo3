60292cb3c77f6b231c550c58fe790935611729f4
Nerijus <nerijus.andriukaitis@gmail.com>
2020-07-08 20:26:25 +0300

    add greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
 
+
+    }
+    public static void greet (String name){
+
+        System.out.println("Hello "+name);
     }
 }
