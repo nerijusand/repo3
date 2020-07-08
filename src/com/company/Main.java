3495487997ee7c04ac00df5c6bab4dd48671d3f4
Nerijus <nerijus.andriukaitis@gmail.com>
2020-07-08 20:34:04 +0300

    print names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+    greet("Nerijus");
+    greet("Tomas");
 
     }
     public static void greet (String name){
