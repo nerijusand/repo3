b4a1c9545143c33f4dde77d49a2799e188f04760
Nerijus <nerijus.andriukaitis@gmail.com>
2020-07-15 18:27:47 +0300

    File uzdaviniai Nr1;2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,49 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-
-        Monitoring monitoring = new Monitoring();
-
-        List<Device> deviceList = new ArrayList<>();
-        deviceList.add(new Printer(true,123454));
-        deviceList.add(new Mouse(true,123455));
-        deviceList.add(new Display(false,121454));
-        monitoring.pingAllDevices(deviceList);
+    public static void main(String[] args) {
+//        uzduotis1();
+        saveDrivers(Paths.get("src", "com", "company", "drivers_out"), Driver.drivers());
     }
 
+    public static void saveDrivers(Path path, List<Driver> drivers) {
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (Driver driver : drivers) {
+                writer.write(driver.toLine());
+                writer.newLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void uzduotis1() {
+        try (  BufferedReader reader = Files.newBufferedReader(Paths.get("src", "com", "company", "vardas"));
+               BufferedWriter writer = Files.newBufferedWriter(Paths.get("src", "com", "company", "vardas_out"))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                writer.write(String.format("%s %s", new StringBuffer(parts[0]).reverse(), new StringBuffer(parts[1]).reverse()));
+                writer.newLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void uzduotis2() {
+
+    }
 }
+
